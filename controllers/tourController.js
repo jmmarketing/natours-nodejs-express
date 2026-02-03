ddf49bea927b2efc6566ae607f607b7194ba4f28
jmmarketing <jeff@jeffreymclean.com>
2026-02-03 15:02:26 -0800

    Built aggregation pipeline for monthlyPlan data in tourController

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -156,9 +156,45 @@ exports.getTourStats = async (req, res) => {
 
 exports.getMonthlyPlan = async (req, res) => {
   try {
+    const year = +req.params.year;
+
+    //Tour.aggregate = mongoose
+    const plan = await Tour.aggregate([
+      //All of this is MongoDB +JS
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      { $project: { _id: 0 } },
+      {
+        $sort: { month: 1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+
+    // Express
     res.status(200).json({
       status: 'success',
-      data: stats,
+      data: plan,
     });
   } catch (err) {
     res.status(404).json({
