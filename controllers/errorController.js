a4267155fd855534e78289fdf9b96dfd312d8c6d
jmmarketing <jeff@jeffreymclean.com>
2026-03-20 16:36:09 -0700

    Created conditionals and functions to handle operational erroers in the globeErrorHandler module (errorController.js). Specifically for CastErrors, Validation Errors, and Duplicated Value errors.

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,25 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+
+  return new AppError(message, 400);
+};
+
+const handleDuplicateValueDB = (err) => {
+  const message = `Duplicate value: ${err.keyValue.name}. Please use another value`;
+
+  return new AppError(message, 409);
+};
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}.`;
+
+  return new AppError(message, 400);
+};
+
 const sendErrorDevelopment = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -7,10 +29,24 @@ const sendErrorDevelopment = (err, res) => {
   });
 };
 const sendErrorProduction = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  //Operational error based from AppError
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    //Programming error and done want to leak details
+  } else {
+    //Log the error to console
+    console.log('ERROR! 🚩', err);
+
+    //Send generic message
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went very wrong!',
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
@@ -22,6 +58,14 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDevelopment(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProduction(err, res);
+    // Need to incldue name:err.name because when destructuring you lose the name property, as it is passed by prototype inheritence.
+    // let error = { ...err, name: err.name};
+    let error = { ...err };
+
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateValueDB(error);
+    if (err.name == 'ValidationError') error = handleValidationErrorDB(error);
+
+    sendErrorProduction(error, res);
   }
 };
