28e8a26cc8a1381f73e86f19ea38688c3b7d0341
jmmarketing <jeff@jeffreymclean.com>
2026-03-20 18:15:32 -0700

    Added uncaughtExceptions and unhandledRejections error handlers in the server.js file.

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 
+process.on('uncaughtException', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION.. Shutting down....');
+  process.exit(1);
+});
+
 const app = require('./app');
 const port = process.env.PORT || 3000;
 
@@ -18,6 +24,17 @@ mongoose.connect(DB).then(() => console.log('DB Connection Successful'));
 // #####################
 // ### LISTENING  ######
 // #####################
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Running on port: ${port}`);
+});
+
+// Handles promise rejectioins (lik DB Connections) - Safetynet
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION.. Shutting down....');
+
+  //Shuts down the app
+  server.close(() => {
+    process.exit(1);
+  });
 });
