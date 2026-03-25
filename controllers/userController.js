6b9a8c0f9fe4a147e04a046c0505fd1842f1a197
jmmarketing <jeff@jeffreymclean.com>
2026-03-25 15:17:38 -0700

    Built out the getAllUsers controller function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,24 @@
-const fs = require('fs');
-const userData = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/users.json`)
-);
+// const fs = require('fs');
+// // const userData = JSON.parse(
+// //   fs.readFileSync(`${__dirname}/../dev-data/data/users.json`)
+// // );
+
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const APIFeatures = require('../utils/apiFeatures');
 
 // <<<<<<<<< USER ROUTE HANDLERS >>>>>>>>>
 
-exports.getAllUsers = (req, res) => {
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const features = new APIFeatures(User.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
+
+  const userData = await features.query;
+
   res.status(200).json({
     status: 'success',
     userCount: userData.length,
@@ -13,7 +26,7 @@ exports.getAllUsers = (req, res) => {
       users: userData,
     },
   });
-};
+});
 
 exports.createUser = (req, res) => {
   res.status(500).json({
