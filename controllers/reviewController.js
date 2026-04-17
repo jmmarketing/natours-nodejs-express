577e5681ed4159a2f731295927c58e7ca24c8f3f
jmmarketing <jeff@jeffreymclean.com>
2026-04-17 15:39:38 -0700

    Started builout of Review routes/controller. Will start with getAllReviews, createReview, and will be on a get/post requests for /api/v1/review/

---
+++ controllers/reviewController.js
@@ -0,0 +1,11 @@
+//Get All Reviews
+//Create Review
+
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
+const APIFeatures = require('./../utils/apiFeatures');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.getAllReview = catchAsync(async (req, res, next) => {});
+
+exports.createReview = catchAsync(async (req, res, next) => {});
